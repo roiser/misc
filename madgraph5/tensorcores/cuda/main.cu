b0b1dcd64203f37cea21c007f5aa0c156d81c6e6
Stefan Roiser <stefan.roiser@cern.ch>
2022-08-05 10:12:44 +0200

    refactor

--- madgraph5/tensorcores/cuda/main.cu
+++ madgraph5/tensorcores/cuda/main.cu
@@ -1,16 +1,16 @@
-// https://www.quantstart.com/articles/Matrix-Matrix-Multiplication-on-the-GPU-with-Nvidia-CUDA/ 
+// https://www.quantstart.com/articles/Matrix-Matrix-Multiplication-on-the-GPU-with-Nvidia-CUDA/
 
 #include <iostream>
 #include <vector>
 #include <stdlib.h>
 #include <time.h>
 #include <cuda_runtime.h>
-#include "kernel.h"
-#include "kernel.cu"
-#include "dev_array.h"
-#include <math.h>
 
-using namespace std;
+#include "kernel.h"
+#include "dev_array.h"
+#include "hst_matrix.h"
+
+#define MTYPE float
 
 int main()
 {
@@ -20,22 +20,17 @@ int main()
     int SIZE = N*N;
 
     // Allocate memory on the host
-    vector<float> h_A(SIZE);
-    vector<float> h_B(SIZE);
-    vector<float> h_C(SIZE);
-
-    // Initialize matrices on the host
-    for (int i=0; i<N; i++){
-        for (int j=0; j<N; j++){
-            h_A[i*N+j] = sin(i);
-            h_B[i*N+j] = cos(j);
-        }
-    }
+    std::vector<MTYPE> h_A(SIZE);
+    std::vector<MTYPE> h_B(SIZE);
+    std::vector<MTYPE> h_C(SIZE);
+    std::vector<MTYPE> cpu_C(SIZE);
 
     // Allocate memory on the device
-    dev_array<float> d_A(SIZE);
-    dev_array<float> d_B(SIZE);
-    dev_array<float> d_C(SIZE);
+    dev_array<MTYPE> d_A(SIZE);
+    dev_array<MTYPE> d_B(SIZE);
+    dev_array<MTYPE> d_C(SIZE);
+
+    matrixInitialize(&h_A, &h_B, N);
 
     d_A.set(&h_A[0], SIZE);
     d_B.set(&h_B[0], SIZE);
@@ -46,30 +41,11 @@ int main()
     d_C.get(&h_C[0], SIZE);
     cudaDeviceSynchronize();
 
-    float *cpu_C;
-    cpu_C=new float[SIZE];
+    cpuMatrixMult(&h_A, &h_B, &cpu_C, N);
 
-    // Now do the matrix multiplication on the CPU
-    float sum;
-    for (int row=0; row<N; row++){
-        for (int col=0; col<N; col++){
-            sum = 0.f;
-            for (int n=0; n<N; n++){
-                sum += h_A[row*N+n]*h_B[n*N+col];
-            }
-            cpu_C[row*N+col] = sum;
-        }
-    }
+    double err = errorCheck(&cpu_C, &h_C, N);
 
-    double err = 0;
-    // Check the result and make sure it is correct
-    for (int ROW=0; ROW < N; ROW++){
-        for (int COL=0; COL < N; COL++){
-            err += cpu_C[ROW * N + COL] - h_C[ROW * N + COL];
-        }
-    }
-
-    cout << "Error: " << err << endl;
+    std::cout << "Error: " << err << std::endl;
 
     return 0;
 }
