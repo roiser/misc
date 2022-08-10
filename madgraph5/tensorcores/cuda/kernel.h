fdc74f2e918ca12f6f734736b910dc418e2e39fd
Stefan Roiser <stefan.roiser@cern.ch>
2022-08-10 09:11:17 +0200

    fix cuda matrix mult

--- madgraph5/tensorcores/cuda/kernel.h
+++ madgraph5/tensorcores/cuda/kernel.h
@@ -7,13 +7,15 @@
 #include <math.h>
 #include <stdlib.h>
 
+// Multiplication on the Cuda cores
+
 template <typename T>
-__global__ void matrixMultiplicationKernel(T *A, T *B, T *C, int N) {
+__global__ void matrixMultiplicationKernelCuda(T *A, T *B, T *C, int N) {
 
   int ROW = blockIdx.y * blockDim.y + threadIdx.y;
   int COL = blockIdx.x * blockDim.x + threadIdx.x;
 
-  float tmpSum = 0;
+  double tmpSum = 0.;
 
   if (ROW < N && COL < N) {
     // each thread computes one element of the block sub-matrix
@@ -24,19 +26,55 @@ __global__ void matrixMultiplicationKernel(T *A, T *B, T *C, int N) {
   C[ROW * N + COL] = tmpSum;
 }
 
-template <typename T> void matrixMultiplication(T *A, T *B, T *C, int N) {
+template <typename T> void matrixMultiplicationCuda(T *A, T *B, T *C, int N) {
   // declare the number of blocks per grid and the number of threads per block
   // use 1 to 512 threads per block
   dim3 threadsPerBlock(N, N);
   dim3 blocksPerGrid(1, 1);
-  if (N * N > 512) {
-    threadsPerBlock.x = 512;
-    threadsPerBlock.y = 512;
-    blocksPerGrid.x = ceil(double(N) / double(threadsPerBlock.x));
-    blocksPerGrid.y = ceil(double(N) / double(threadsPerBlock.y));
+  if (N * N > 1024) {
+    threadsPerBlock.x = 32;
+    threadsPerBlock.y = 32;
+    blocksPerGrid.x = ceil(double(N) / double(32));
+    blocksPerGrid.y = ceil(double(N) / double(32));
   }
 
-  matrixMultiplicationKernel<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
+  matrixMultiplicationKernelCuda<<<blocksPerGrid, threadsPerBlock>>>(A, B, C,
+                                                                     N);
+}
+
+// Multiplication on the tensor cores
+
+template <typename T>
+__global__ void matrixMultiplicationKernelTensor(T *A, T *B, T *C, int N) {
+
+  int ROW = blockIdx.y * blockDim.y + threadIdx.y;
+  int COL = blockIdx.x * blockDim.x + threadIdx.x;
+
+  double tmpSum = 0.;
+
+  if (ROW < N && COL < N) {
+    // each thread computes one element of the block sub-matrix
+    for (int i = 0; i < N; i++) {
+      tmpSum += A[ROW * N + i] * B[i * N + COL];
+    }
+  }
+  C[ROW * N + COL] = tmpSum;
+}
+
+template <typename T> void matrixMultiplicationTensor(T *A, T *B, T *C, int N) {
+  // declare the number of blocks per grid and the number of threads per block
+  // use 1 to 512 threads per block
+  dim3 threadsPerBlock(N, N);
+  dim3 blocksPerGrid(1, 1);
+  if (N * N > 1024) {
+    threadsPerBlock.x = 32;
+    threadsPerBlock.y = 32;
+    blocksPerGrid.x = ceil(double(N) / double(32));
+    blocksPerGrid.y = ceil(double(N) / double(32));
+  }
+
+  matrixMultiplicationKernelTensor<<<blocksPerGrid, threadsPerBlock>>>(A, B, C,
+                                                                       N);
 }
 
 #endif
