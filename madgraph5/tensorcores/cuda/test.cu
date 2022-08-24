46520136da7c9b03f2c412875c0c86f4d99fdc9c
Stefan Roiser <stefan.roiser@cern.ch>
2022-08-24 15:15:20 +0200

    move data out of kernel

--- madgraph5/tensorcores/cuda/test.cu
+++ madgraph5/tensorcores/cuda/test.cu
@@ -1,4 +1,5 @@
 
+#include "dev_array.h"
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <iostream>
@@ -7,65 +8,68 @@
 
 using namespace nvcuda;
 
-__global__ void mult() {
+__global__ void mult(const double *A, const double *B, double *C, int N) {
 
-  printf("kernel\n");
-
-  double A[64] = {1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8,
-                  1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8,
-                  1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8,
-                  1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
-  double B[64] = {8, 7, 6, 5, 4, 3, 2, 1, 8, 7, 6, 5, 4, 3, 2, 1,
-                  8, 7, 6, 5, 4, 3, 2, 1, 8, 7, 6, 5, 4, 3, 2, 1,
-                  8, 7, 6, 5, 4, 3, 2, 1, 8, 7, 6, 5, 4, 3, 2, 1,
-                  8, 7, 6, 5, 4, 3, 2, 1, 8, 7, 6, 5, 4, 3, 2, 1};
-  double C[64] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  printf("kernel start\n");
 
   wmma::fragment<wmma::matrix_a, 8, 8, 4, double, wmma::row_major> a_frag;
   wmma::fragment<wmma::matrix_b, 8, 8, 4, double, wmma::row_major> b_frag;
   wmma::fragment<wmma::accumulator, 8, 8, 4, double> acc_frag;
-  // wmma::fragment<wmma::accumulator, 8, 8, 4, double> c_frag;
-  wmma::fill_fragment(acc_frag, 0.0f);
 
   wmma::load_matrix_sync(a_frag, A, 8);
   wmma::load_matrix_sync(b_frag, B, 8);
+  wmma::fill_fragment(acc_frag, 0.);
 
   wmma::mma_sync(acc_frag, a_frag, b_frag, acc_frag);
 
-  wmma::store_matrix_sync(C, acc_frag, 8, wmma::mem_row_major);
+  wmma::store_matrix_sync(C, acc_frag, N, wmma::mem_row_major);
 
-  printf("%f, %f, %f, %f, %f, %f, %f, %f\n", C[0], C[1], C[2], C[3], C[4], C[5],
-         C[6], C[7]);
+  printf("kernel stop\n");
 }
 
 int main() {
 
   std::cout << "start" << std::endl;
 
-  mult<<<1, 1>>>();
+  int N = 8;
+  int SIZE = N * N;
+
+  double A[SIZE] = {1., 2., 3., 4., 5., 6., 7., 8., 1., 2., 3., 4., 5.,
+                    6., 7., 8., 1., 2., 3., 4., 5., 6., 7., 8., 1., 2.,
+                    3., 4., 5., 6., 7., 8., 1., 2., 3., 4., 5., 6., 7.,
+                    8., 1., 2., 3., 4., 5., 6., 7., 8., 1., 2., 3., 4.,
+                    5., 6., 7., 8., 1., 2., 3., 4., 5., 6., 7., 8.};
+  double B[SIZE] = {8., 7., 6., 5., 4., 3., 2., 1., 8., 7., 6., 5., 4.,
+                    3., 2., 1., 8., 7., 6., 5., 4., 3., 2., 1., 8., 7.,
+                    6., 5., 4., 3., 2., 1., 8., 7., 6., 5., 4., 3., 2.,
+                    1., 8., 7., 6., 5., 4., 3., 2., 1., 8., 7., 6., 5.,
+                    4., 3., 2., 1., 8., 7., 6., 5., 4., 3., 2., 1.};
+  double C[SIZE] = {0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+                    0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+                    0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+                    0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+                    0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.};
+
+  dev_array<double> d_A(SIZE);
+  dev_array<double> d_B(SIZE);
+  dev_array<double> d_C(SIZE);
+
+  d_A.set(&A[0], SIZE);
+  d_B.set(&B[0], SIZE);
+
+  mult<<<1, 1>>>(d_A.getData(), d_B.getData(), d_C.getData(), N);
   cudaDeviceSynchronize();
+  d_C.get(&C[0], SIZE);
+  cudaDeviceSynchronize();
+
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      std::cout << C[i * N + j] << ", ";
+    }
+    std::cout << std::endl;
+  }
 
   std::cout << "stop" << std::endl;
 
   return 0;
 }
-
-/*
-
-#include <assert.h>
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <stdio.h>
-
-__global__ void test() { printf("Hi Cuda World\n"); }
-
-int main(int argc, char **argv) {
-  test<<<1, 1>>>();
-  cudaDeviceSynchronize();
-  return 0;
-}
-
-*/
