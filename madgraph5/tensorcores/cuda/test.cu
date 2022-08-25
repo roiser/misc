81c727622490a744dad75e17708013c4d40546d7
Stefan Roiser <stefan.roiser@cern.ch>
2022-08-25 10:57:25 +0200

    make it generic, 1st step

--- madgraph5/tensorcores/cuda/test.cu
+++ madgraph5/tensorcores/cuda/test.cu
@@ -1,10 +1,25 @@
-
 #include "dev_array.h"
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <iostream>
 #include <mma.h>
 #include <stdlib.h>
+
+// #define B_is_row_major
+
+#ifdef B_is_row_major
+#define __BRM_mjr__ wmma::row_major // wmma name
+#define __BRM_cdm__ M               // column dimension
+#define __BRM_rdm__ K               // row dimension
+#define __BRM_mat__ B_rm            // matrix name
+#define __BRM_idx__ j               // index var for fill
+#else
+#define __BRM_mjr__ wmma::col_major
+#define __BRM_cdm__ K
+#define __BRM_rdm__ M
+#define __BRM_mat__ B_cm
+#define __BRM_idx__ i
+#endif
 
 /*
 pointers
@@ -17,84 +32,81 @@ using namespace nvcuda;
 constexpr int M = 8, N = 8, K = 4;
 
 __global__ void mult(const double *A, const double *B, double *C) {
-
-  // printf("kernel start\n");
-
   wmma::fragment<wmma::matrix_a, M, N, K, double, wmma::row_major> a_frag;
-  // row_major or col_major
-  wmma::fragment<wmma::matrix_b, M, N, K, double, wmma::col_major> b_frag;
+  wmma::fragment<wmma::matrix_b, M, N, K, double, __BRM_mjr__>
+      b_frag; // row/col
   wmma::fragment<wmma::accumulator, M, N, K, double> c_frag;
 
   wmma::load_matrix_sync(a_frag, A, K);
-  wmma::load_matrix_sync(b_frag, B, K); // row-major: M, col-major: K
+  wmma::load_matrix_sync(b_frag, B, __BRM_cdm__); // row-major: M, col-major: K
   wmma::fill_fragment(c_frag, 0.);
 
   wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
 
   wmma::store_matrix_sync(C, c_frag, N, wmma::mem_row_major);
+}
 
-  // printf("kernel stop\n");
+void fill(double A[], double B[], double C[]) {
+  for (int i = 0; i < M; ++i)
+    for (int j = 0; j < K; ++j)
+      A[i * K + j] = j + 1;
+
+  for (int i = 0; i < __BRM_rdm__; ++i)
+    for (int j = 0; j < __BRM_cdm__; ++j)
+      B[i * __BRM_cdm__ + j] = __BRM_idx__ + 1;
+
+  for (int i = 0; i < M; ++i)
+    for (int j = 0; j < N; ++j)
+      C[i * N + j] = 0;
+}
+
+void print(double A[], double B[], double C[]) {
+
+  std::cout << "Matrix A" << std::endl;
+  for (int i = 0; i < M; ++i) {
+    for (int j = 0; j < K; ++j) {
+      std::cout << A[i * K + j] << ", ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+
+  std::cout << "Matrix B" << std::endl;
+  for (int i = 0; i < __BRM_rdm__; ++i) {
+    for (int j = 0; j < __BRM_cdm__; ++j) {
+      std::cout << B[i * __BRM_cdm__ + j] << ", ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+
+  std::cout << "Matrix C" << std::endl;
+  for (int i = 0; i < M; ++i) {
+    for (int j = 0; j < N; ++j) {
+      std::cout << C[i * N + j] << ", ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
 }
 
 int main() {
 
-  std::cout << "start" << std::endl;
-
   const int SA = M * K, SB = K * N, SC = M * N;
+  double A_rm[SA], __BRM_mat__[SB], C_rm[SC];
+  dev_array<double> d_A(SA), d_B(SB), d_C(SC);
 
-  // clang-format off
-  double A[SA] = {1., 2., 3., 4.,
-                  1., 2., 3., 4.,
-                  1., 2., 3., 4.,
-                  1., 2., 3., 4.,
-                  1., 2., 3., 4.,
-                  1., 2., 3., 4.,
-                  1., 2., 3., 4.,
-                  1., 2., 3., 4.};
-  // row-major
-  // double B[SB] = {8., 7., 6., 5., 4., 3., 2., 1.,
-  //                 8., 7., 6., 5., 4., 3., 2., 1.,
-  //                 8., 7., 6., 5., 4., 3., 2., 1.,
-  //                 8., 7., 6., 5., 4., 3., 2., 1.};
-  // col-major
-  double B[SB] = {8., 8., 8., 8.,
-                  7., 7., 7., 7.,
-                  6., 6., 6., 6.,
-                  5., 5., 5., 5.,
-                  4., 4., 4., 4.,
-                  3., 3., 3., 3.,
-                  2., 2., 2., 2.,
-                  1., 1., 1., 1.};
-  double C[SC] = {0., 0., 0., 0., 0., 0., 0., 0.,
-                  0., 0., 0., 0., 0., 0., 0., 0.,
-                  0., 0., 0., 0., 0., 0., 0., 0.,
-                  0., 0., 0., 0., 0., 0., 0., 0.,
-                  0., 0., 0., 0., 0., 0., 0., 0.,
-                  0., 0., 0., 0., 0., 0., 0., 0.,
-                  0., 0., 0., 0., 0., 0., 0., 0.,
-                  0., 0., 0., 0., 0., 0., 0., 0.};
-// clang-format once
+  fill(A_rm, __BRM_mat__, C_rm);
 
-  dev_array<double> d_A(SA);
-  dev_array<double> d_B(SB);
-  dev_array<double> d_C(SC);
-
-  d_A.set(A, SA);
-  d_B.set(B, SB);
+  d_A.set(A_rm, SA);
+  d_B.set(__BRM_mat__, SB);
 
   mult<<<1, 32>>>(d_A.getData(), d_B.getData(), d_C.getData());
   cudaDeviceSynchronize();
-  d_C.get(C, SC);
+  d_C.get(C_rm, SC);
   cudaDeviceSynchronize();
 
-  for (int i = 0; i < M; ++i) {
-    for (int j = 0; j < N; ++j) {
-      std::cout << C[i * M + j] << ", ";
-    }
-    std::cout << std::endl;
-  }
-
-  std::cout << "stop" << std::endl;
+  print(A_rm, __BRM_mat__, C_rm);
 
   return 0;
 }
