f626b1144ea071fb253b6f5d8162c650fd7de247
Stefan Roiser <stefan.roiser@cern.ch>
2022-09-02 16:28:17 +0200

    fix macros

--- madgraph5/tensorcores/cuda/matrixelem/macros.h
+++ madgraph5/tensorcores/cuda/matrixelem/macros.h
@@ -5,17 +5,17 @@
 #define B_is_row_major
 
 #ifdef A_is_row_major
-#define _A_mjr_ wmma::col_major
-#define _A_cdm_ M
-#define _A_rdm_ K
-#define _A_mat_ A_cm
-#define _A_idx_ i
-#else
 #define _A_mjr_ wmma::row_major // wmma name
 #define _A_cdm_ K               // column dimension
 #define _A_rdm_ M               // row dimension
 #define _A_mat_ A_rm            // matrix name
 #define _A_idx_ j               // index var for fill
+#else
+#define _A_mjr_ wmma::col_major
+#define _A_cdm_ M
+#define _A_rdm_ K
+#define _A_mat_ A_cm
+#define _A_idx_ i
 #endif
 
 #ifdef B_is_row_major
