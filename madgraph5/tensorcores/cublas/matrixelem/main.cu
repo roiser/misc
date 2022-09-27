7a604199c394fcccf48299a05cfc71c300372c22
Stefan Roiser <stefan.roiser@cern.ch>
2022-09-27 11:01:04 +0200

    first try

--- madgraph5/tensorcores/cublas/matrixelem/main.cu
+++ madgraph5/tensorcores/cublas/matrixelem/main.cu
@@ -2,8 +2,13 @@
 #include "hst_matrix.h"
 #include "kernel.h"
 
-#define MG5EXAMPLE
-//#define CUBLAS
+#include <cuComplex.h>
+//#include <cublas.h>
+//#include <cublas_api.h>
+#include <cublas_v2.h>
+
+//#define MG5EXAMPLE
+#define CUBLAS
 
 /*
 Docu
@@ -32,6 +37,31 @@ int main() {
   print(_A_mat_, _B_mat_, C_rm, _A_rdm_, _A_cdm_, _B_rdm_, _B_cdm_, M, N, K);
 
 #elif defined(CUBLAS)
+
+  // https://docs.nvidia.com/cuda/cublas/index.html#cublas-lt-t-gt-gemv
+
+  // Create a handle for CUBLAS
+  cublasHandle_t handle;
+  cublasCreate(&handle);
+
+  cublasOperation_t trans = CUBLAS_OP_N;
+  int m = 8, n = 8, lda = 0, incx = 0, incy = 0;
+  cuDoubleComplex *alpha = 0, *A = 0, *x = 0, *beta = 0, *y = 0;
+
+  // cublasHandle_t handle,
+  // cublasOperation_t trans,
+  // int m, int n,
+  // const cuDoubleComplex *alpha,
+  // const cuDoubleComplex *A, int lda,
+  // const cuDoubleComplex *x, int incx,
+  // const cuDoubleComplex *beta,
+  // cuDoubleComplex *y, int incy
+
+  // Do the actual multiplication
+  cublasZgemv(handle, trans, m, n, alpha, A, lda, x, incx, beta, y, incy);
+
+  // Destroy the handle
+  cublasDestroy(handle);
 
 #else  // simple example
   const int M = 8, N = 8, K = 4, SA = M * K, SB = K * N, SC = M * N;
